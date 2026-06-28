52edfc195703bdc3616770afa2f7843baade493d
devstiven12 <devstiven12@gmail.com>
2026-06-28 14:58:31 -0500

    se crea el taller

---
+++ Taller/src/test/java/com/MaikNasP/Taller/TallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MaikNasP.Taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
