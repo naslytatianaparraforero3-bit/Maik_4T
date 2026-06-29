2a1ad5ee375c8773f975df062caa4f5d5a6930c7
devstiven12 <devstiven12@gmail.com>
2026-06-29 16:51:54 -0500

    se crean los dto :)

--- Taller/src/main/java/com/MaikNasP/Taller/dto/TareaRequestDto.java
+++ Taller/src/main/java/com/MaikNasP/Taller/dto/TareaRequestDto.java
@@ -1,5 +1,23 @@
 package com.MaikNasP.Taller.dto;
 
+import com.MaikNasP.Taller.entity.PrioridadTarea;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TareaRequestDto {
-    
+
+    @NotBlank(message = "El título es obligatorio")
+    @Size(min = 3, message = "El título debe tener al menos 3 caracteres")
+    private String titulo;
+
+    private String descripcion;
+
+    @NotNull(message = "La prioridad es obligatoria")
+    private PrioridadTarea prioridad;
 }
--- Taller/src/main/java/com/MaikNasP/Taller/dto/TareaResponseDto.java
+++ Taller/src/main/java/com/MaikNasP/Taller/dto/TareaResponseDto.java
@@ -1,5 +1,21 @@
 package com.MaikNasP.Taller.dto;
 
+import com.MaikNasP.Taller.entity.EstadoTarea;
+import com.MaikNasP.Taller.entity.PrioridadTarea;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TareaResponseDto {
-    
+
+    private Long id;
+    private String titulo;
+    private String descripcion;
+    private EstadoTarea estado;
+    private PrioridadTarea prioridad;
+    private LocalDateTime fechaCreacion;
+    private LocalDateTime fechaActualizacion;
 }
