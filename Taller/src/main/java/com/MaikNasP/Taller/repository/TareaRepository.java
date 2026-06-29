1408369c1363571dc2ffd8501330ca1d8f78c66b
devstiven12 <devstiven12@gmail.com>
2026-06-29 18:36:19 -0500

    se crea el reposi :/

--- Taller/src/main/java/com/MaikNasP/Taller/repository/TareaRepository.java
+++ Taller/src/main/java/com/MaikNasP/Taller/repository/TareaRepository.java
@@ -1,5 +1,19 @@
 package com.MaikNasP.Taller.repository;
 
-public class TareaRepository {
-    
+import com.MaikNasP.Taller.entity.EstadoTarea;
+import com.MaikNasP.Taller.entity.PrioridadTarea;
+import com.MaikNasP.Taller.entity.Tarea;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TareaRepository extends JpaRepository<Tarea, Long> {
+
+    List<Tarea> findByEstado(EstadoTarea estado);
+
+    List<Tarea> findByPrioridad(PrioridadTarea prioridad);
+
+    List<Tarea> findByTituloContainingIgnoreCase(String titulo);
 }
